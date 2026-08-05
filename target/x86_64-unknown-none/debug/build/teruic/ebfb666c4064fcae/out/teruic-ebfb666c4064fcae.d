6693373654b40657cd55eb617ecdeab8ace2e17a
Anmin-bhu <anminnbhusal@gmail.com>
2026-08-05 21:51:19 +0545

    changed dependecy of x86_64 default features to false

---
+++ target/x86_64-unknown-none/debug/build/teruic/ebfb666c4064fcae/out/teruic-ebfb666c4064fcae.d
@@ -0,0 +1,9 @@
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/ebfb666c4064fcae\out\teruic-ebfb666c4064fcae.d: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\vga.rs
+
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/ebfb666c4064fcae\out\libteruic-ebfb666c4064fcae.rmeta: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\vga.rs
+
+src\main.rs:
+src\interrupts.rs:
+src\print.rs:
+src\serial.rs:
+src\vga.rs:
---
+++ target/x86_64-unknown-none/debug/build/volatile/6b2cf0cf1fd7d718/out/volatile-6b2cf0cf1fd7d718.d
@@ -0,0 +1,8 @@
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\volatile/6b2cf0cf1fd7d718\out\volatile-6b2cf0cf1fd7d718.d: C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\lib.rs C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\access.rs
+
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\volatile/6b2cf0cf1fd7d718\out\libvolatile-6b2cf0cf1fd7d718.rlib: C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\lib.rs C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\access.rs
+
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\volatile/6b2cf0cf1fd7d718\out\libvolatile-6b2cf0cf1fd7d718.rmeta: C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\lib.rs C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\access.rs
+
+C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\lib.rs:
+C:\Users\Anmin_11\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\volatile-0.4.6\src\access.rs:
