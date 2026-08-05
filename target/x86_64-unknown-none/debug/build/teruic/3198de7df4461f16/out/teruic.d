a39330a328c826011c7baa9f849033722fad1941
Anmin-bhu <anminnbhusal@gmail.com>
2026-08-05 22:44:47 +0545

    Updated much more and a lot of progress:
    Shell testing passed and shell built
    and also core OS code writing completed

--- target/x86_64-unknown-none/debug/build/teruic/3198de7df4461f16/out/teruic.d
+++ target/x86_64-unknown-none/debug/build/teruic/3198de7df4461f16/out/teruic.d
@@ -1,9 +1,10 @@
-C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/3198de7df4461f16\out\teruic.d: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\vga.rs
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/3198de7df4461f16\out\teruic.d: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\shell.rs src\vga.rs
 
-C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/3198de7df4461f16\out\teruic: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\vga.rs
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/3198de7df4461f16\out\teruic: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\shell.rs src\vga.rs
 
 src\main.rs:
 src\interrupts.rs:
 src\print.rs:
 src\serial.rs:
+src\shell.rs:
 src\vga.rs:
--- target/x86_64-unknown-none/debug/build/teruic/c48f1786429a6da9/out/teruic-c48f1786429a6da9.d
+++ target/x86_64-unknown-none/debug/build/teruic/c48f1786429a6da9/out/teruic-c48f1786429a6da9.d
@@ -1,10 +1,10 @@
-C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/c48f1786429a6da9\out\teruic-c48f1786429a6da9.d: src\main.rs src\interrupts.rs src\print.rs src\shell.rs src\serial.rs src\vga.rs
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/c48f1786429a6da9\out\teruic-c48f1786429a6da9.d: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\shell.rs src\vga.rs
 
-C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/c48f1786429a6da9\out\libteruic-c48f1786429a6da9.rmeta: src\main.rs src\interrupts.rs src\print.rs src\shell.rs src\serial.rs src\vga.rs
+C:\Users\Anmin_11\Desktop\project\teruic\Teruic\target\x86_64-unknown-none\debug\build\teruic/c48f1786429a6da9\out\libteruic-c48f1786429a6da9.rmeta: src\main.rs src\interrupts.rs src\print.rs src\serial.rs src\shell.rs src\vga.rs
 
 src\main.rs:
 src\interrupts.rs:
 src\print.rs:
-src\shell.rs:
 src\serial.rs:
+src\shell.rs:
 src\vga.rs:
